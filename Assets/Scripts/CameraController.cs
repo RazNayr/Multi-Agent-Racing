d9a1301baa45d1b63b8efb1960b9e37e1e51693a
Ryan Camilleri <45541364+RazNayr@users.noreply.github.com>
2020-12-22 13:27:29 +0100

    Initial common environment
    - Testing for play mode corruption

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform objectToFollow;
+    public Vector3 offset;
+    public float followSpeed = 10;
+    public float lookSpeed = 10;
+
+    public void LookAtTarget()
+    {
+        Vector3 lookDirection = objectToFollow.position - transform.position;
+        Quaternion rotation = Quaternion.LookRotation(lookDirection, Vector3.up);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, lookSpeed * Time.deltaTime);
+    }
+    public void MoveToTarget()
+    {
+        Vector3 targetPosition = objectToFollow.position +
+            objectToFollow.forward * offset.z +
+            objectToFollow.right * offset.x +
+            objectToFollow.up * offset.y;
+
+        transform.position = Vector3.Lerp(transform.position, targetPosition, followSpeed * Time.deltaTime);
+
+    }
+
+    private void FixedUpdate()
+    {
+        LookAtTarget();
+        MoveToTarget();
+    }
+
+}
---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    [Header("Wheel Colliders")]
+    public WheelCollider wheelFrontRight;
+    public WheelCollider wheelFrontLeft;
+    public WheelCollider wheelRearRight;
+    public WheelCollider wheelRearLeft;
+
+    [Header("Wheel Transforms")]
+    public Transform transformWheelFrontRight;
+    public Transform  transformWheelFrontLeft;
+    public Transform transformWheelRearRight;
+    public Transform transformWheelRearLeft;
+
+    [Header("Car Properties")]
+    public float maxSteerAngle = 30;
+    public float speed = 50;
+
+    private float horizontalInput;
+    private float verticalInput;
+    private float steerAngle;
+
+    private void Start()
+    {
+        Rigidbody carRigidbody = GetComponent<Rigidbody>();
+        carRigidbody.velocity = new Vector3(0, 0, -30);
+    }
+
+    private void FixedUpdate()
+    {
+        GetInput();
+        Steer();
+        Accelerate();
+        UpdateWheelPoses();
+    }
+
+    public void GetInput()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+    }
+
+    private void Steer()
+    {
+        steerAngle = maxSteerAngle * horizontalInput;
+        wheelFrontLeft.steerAngle = steerAngle;
+        wheelFrontRight.steerAngle = steerAngle;
+    }
+
+    private void Accelerate()
+    {
+        wheelFrontLeft.motorTorque = verticalInput * speed;
+        wheelFrontRight.motorTorque = verticalInput * speed;
+    }
+
+    private void UpdateWheelPoses()
+    {
+        UpdateWheelPose(wheelFrontLeft, transformWheelFrontLeft);
+        UpdateWheelPose(wheelFrontRight, transformWheelFrontRight);
+        UpdateWheelPose(wheelRearLeft, transformWheelRearLeft);
+        UpdateWheelPose(wheelRearRight, transformWheelRearRight);
+    }
+
+    private void UpdateWheelPose(WheelCollider wheelCollider, Transform wheelTransform)
+    {
+        Vector3 newWheelPosition;
+        Quaternion newWheelRotation;
+
+        wheelCollider.GetWorldPose(out newWheelPosition, out newWheelRotation);
+
+        wheelTransform.position = newWheelPosition;
+        wheelTransform.rotation = newWheelRotation;
+
+    }
+}
